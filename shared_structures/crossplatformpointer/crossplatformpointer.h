d62ac7d6881a93b6a29009fb4569dc7721cb51bb
Chris Woods <6069113+woodsmc@users.noreply.github.com>
2025-10-12 15:21:06 +0100

    Added license and copyright notice

--- shared_structures/crossplatformpointer/crossplatformpointer.h
+++ shared_structures/crossplatformpointer/crossplatformpointer.h
@@ -1,7 +1,27 @@
+/*
+   Copyright 2025 Chris Woods - https://withbighair.com
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 #pragma once
 #include <stdint.h>
 
-// Borrowing concepts from - https://iafisher.com/blog/2020/06/type-safe-generics-in-c
+/*
+Note: I refer the reader the following blog posts:
+- https://iafisher.com/blog/2020/06/type-safe-generics-in-c
+- https://withbighair.com
+*/
 
 #define CONCAT(A, B) A##B
 #define MONIKER _XPLATFORM
--- shared_structures/example/pointer.c
+++ shared_structures/example/pointer.c
@@ -5,7 +5,32 @@ xuint32Ptr u32pr;
 
 uint32_t val = 42;
 
+typedef union TXPointer {
+    void* ptr;
+    uint64_t padding;
+} TXPointer;
+
+typedef struct TCachedXPointer {
+    TXPointer wasm;
+    TXPointer host;
+} TCachedXPointer;
+
+typedef struct TLinkedListNode TLinkedListNode;
+CREATE_CACHED_XPLATFORM(TLinkedListNode, XTLinkedListNode);
+
+typedef struct TLinkedListNode {
+    XTLinkedListNode next;
+    uint32_t value;
+} TLinkedListNode;
+
 int main(int argc, const char* argv[]) {
+    uint32_t value = 42;
+    TCachedXPointer cp = {0};
+    XPTR(cp) = &value;
+
+    printf("value is %u\n", *(uint32_t*)XPTR(cp));
+
+
     SET_NULL(u32pr);
     XPTR(u32pr) = NULL;
     printf("A small example to show cross platform pointers\n");
