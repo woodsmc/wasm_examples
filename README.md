0c0abf63c00c15be5e99c3ced3b6c29ddb1b7a52
Chris Woods <6069113+woodsmc@users.noreply.github.com>
2025-10-17 11:39:11 +0800

    Adding (c) notices

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# Wasm Examples
-An repository of examples to follow withbighair's blog
+# WebAssembly Examples
+This is a collection of sample code used in  [withbighair's blog](https://withbighair.com/).
+
 * Exporting functions from Wasm and calling them from a host
 * Importing a functions from the host environment and calling them from Wasm
+* Understanding External References
+* Function Pointers
+* Shared binary compatible c structures between multiple architectures (handy for zero copy data sharing between WebAssembly and the host)
+
+This all provi
--- shared_structures/example/crossingboundary/crossingboundary_native.c
+++ shared_structures/example/crossingboundary/crossingboundary_native.c
@@ -1,11 +1,25 @@
+/*
+   Copyright 2025 Chris Woods - https://withbighair.com
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 #include "crossingboundary.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include "wasm_export.h"
 
 void update_native_addresses(wasm_module_inst_t module_inst, XTLinkedListNodePtr* head) {
-    // In a real application, you would have a way to map the wasm pointers to native pointers.
-    // For this example, we will assume that the wasm pointers are valid native pointers.
     XTLinkedListNodePtr* current = head;
     
     while (!XPTR_IS_COMPLETELY_NULL(*current)) {
